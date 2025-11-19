624bae53f0329bc97cc3b0ac18fab203990dbac0
Laura Bécognée <jaimaille@gmail.com>
2025-11-19 12:57:45 +0100

    Update `Asset` schema to include `AssetDescription` and enhance `UserAssets`
    - Changed `AssetId` in `Asset` table to `VARCHAR(100)`.
    - Added `AssetDescription` column to `Asset` table.
    - Removed seed data from `Asset` table.
    - Updated `Models/Asset` to add `AssetDescription` property.
    - Added `Orientation` property to `UserAssets` model.

--- Models/Asset.cs
+++ Models/Asset.cs
@@ -3,6 +3,7 @@
 public class Asset {
     public required string AssetId { get; set; }
     public required string AssetName { get; set; }
+    public required string AssetDescription { get; set; }
     public required string AssetCategory { get; set; }
     public int Cost { get; set; }
 }
@@ -11,4 +12,5 @@ public class UserAssets {
     public required string UserId { get; set; }
     public required string AssetId { get; set; }
     public required string Coordinates { get; set; }
+    public required string Orientation { get; set; } // quaternion
 }
